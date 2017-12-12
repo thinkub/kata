c2430118ceef02bdc113a2d99b286a8c6ca25553
이민호/TL개발팀 <thinkub0219@gmail.com>
2017-12-12 21:30:44 +0900

    [6kyu] Your order, please
    https://www.codewars.com/kata/55c45be3b2079eccff00010f/train/java

---
+++ src/main/java/Order.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @author by Ming(thinkub0219@gmail.com) on 2017. 12. 11..
+ */
+public class Order {
+    public static String order(String words) {
+        if (words.replaceAll("[^0-9]", "").length() == 0) {
+            return "";
+        }
+
+        List<String> lists = Arrays.stream(words.split(" ")).collect(Collectors.toList());
+        Map<Integer, Integer> listsIndexMap = new HashMap<>(lists.size());
+
+        for (int i = 0; i < lists.size(); i++) {
+            listsIndexMap.put(Integer.valueOf(lists.get(i).replaceAll("[^0-9]", "")), i);
+        }
+
+        List<String> resultList = new ArrayList<>(lists.size());
+        for (int i = 0; i < lists.size(); i++) {
+            resultList.add(lists.get(listsIndexMap.get(i + 1)));
+        }
+        return String.join(" ", resultList);
+    }
+}
